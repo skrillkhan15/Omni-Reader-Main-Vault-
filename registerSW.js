ea231bcf7e76d882dc083046ad6ff0d9eccb282c
skrillkhan15 <skrill0010@gmail.com>
2025-08-20 18:15:35 +0600

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Omni-Reader-Main-Vault-/sw.js', { scope: '/Omni-Reader-Main-Vault-/' })})}
